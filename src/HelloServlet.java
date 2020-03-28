05cbcdbd96ecd2b0009bd8bc107791f687f0a511
1419526625@qq.com <1419526625@qq.com>
2020-03-28 17:56:57 +0800

    Pull之后需要重新配置Tomcat运行 浏览器地址栏输入:localhost:8080/hello

--- src/HelloServlet.java
+++ src/HelloServlet.java
@@ -8,7 +8,7 @@ public class HelloServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) {
 
         try {
-            response.getWriter().println("<h1>Hello Servlet!</h1>");
+            response.getWriter().println("<h1>Hello Servlet!!!</h1>");
             response.getWriter().println(new Date().toLocaleString());
         } catch (IOException e) {
             // TODO Auto-generated catch block
